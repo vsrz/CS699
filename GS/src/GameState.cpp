37d1f2e4fe965c180101ef1561fc982531bd58d1
Jeremy Villegas <ritual@vsritual.com>
2014-06-10 21:08:31 -0700

    Remove the mouse click orbs

--- GS/src/GameState.cpp
+++ GS/src/GameState.cpp
@@ -84,7 +84,9 @@ bool GameState::handleEvent(const sf::Event& event)
 	{
 		// Send the click to the world to find out which tile was clicked
 		sf::Vector2i mousePos = sf::Mouse::getPosition(*getContext().window);
-		mClick.addClickEvent(snapToSpritePosition(mousePos));
+		
+		// Remove the circle click event based on the new notification system
+		//mClick.addClickEvent(snapToSpritePosition(mousePos));
 	}
 
 	return true;
